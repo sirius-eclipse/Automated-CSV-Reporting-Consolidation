36f5f45ceda5f07158da8a13fbae275952876500
Mihail Barbuta <black.myke@gmail.com>
2011-05-16 16:27:45 +0300

    Implemented dynamic paging

--- CSV_Reporting/src/CSV_ReportingConsolidator.java
+++ CSV_Reporting/src/CSV_ReportingConsolidator.java
@@ -2,7 +2,7 @@
  *  Name: CSV Performance Test report consolidating automation utility
  *  Author: Barbuta Mihail-Gabriel - barbuta.mihail@ro.ibm.com
  *  Company: IBM Romania - Global Delivery Center Bucharest
- *  Date: 2011-05-15 / Revision: 1.3
+ *  Date: 2011-05-15 / Revision: 1.5
  *  
  *  Developed during Yahoo! Open Hack, Bucharest, 14-15 May 2011
  *	
@@ -15,8 +15,11 @@
  *==============================================================================================
  */
 
-
 import java.io.*;
+import java.util.Arrays;
+
+import org.apache.commons.lang.ArrayUtils;
+
 import au.com.bytecode.opencsv.CSVReader;	// Open-source, commercial-friendly licensed
 import au.com.bytecode.opencsv.CSVWriter;	// 	libraries for handling CSV files
 
@@ -25,38 +28,42 @@ public class CSV_ReportingConsolidator {
 	public static void main(String[] args) throws IOException {
 
 		// Construct an array containing the list of files in the input folder
-		String path = "input/"; 					// Set the directory containing the CSV files
-		File folder = new File(path);				// Load the selected path
+		String inputPath = "input/"; 				// Set the directory containing the CSV files
+		String outputPath = "output/";				// Set the output directory for the consolidated report
+		String outputFile = "Consolidated_CSV_Report.csv";
+		File folder = new File(inputPath);			// Load the selected path
 		File[] listOfFiles = folder.listFiles(); 	// Retrieve the list of files from the directory
 		
 		// Serialize the reference headers to write the output CSV header
 		CSVReader referenceReader = new CSVReader(new FileReader("reference/example_fields.csv"));
 		String [] referenceHeaders = referenceReader.readNext();
-		CSVWriter writer = new CSVWriter(new FileWriter("output/Consolidated_CSV_Report.csv"), ',', CSVWriter.NO_QUOTE_CHARACTER);
-		writer.writeNext(referenceHeaders);
+		CSVWriter writer = new CSVWriter(new FileWriter(outputPath+outputFile), ',', CSVWriter.NO_QUOTE_CHARACTER);
 		
 		for (int i = 0; i < listOfFiles.length; i++) 
 		{
 			if (listOfFiles[i].isFile()) {
 				String filename = listOfFiles[i].getName();		// Retrieve the file name
-				System.out.println("== Processed: " + filename + " ===========================");
 				
 				if (!filename.endsWith("csv"))	{				// Check if the file has a CSV extension
 					System.out.println("The input path contains non-csv files. Please remove them and try again.");
+					writer.close();
 					System.exit(1);								// Exit if non-CSV files are found
 				}
 				
-				String filePath=String.valueOf(path+filename);	// Combine the path with the filename
+				String filePath=String.valueOf(inputPath+filename);	// Combine the path with the filename
 				File file = new File(filePath);
 				CSVReader csvFile = new CSVReader(new FileReader(filePath));
-				String [] nextLine;								// CSV line data container
-				String [] Page = {"","","","","","","","",""};	// Holder for Page names
-				double PagePRT[] = {0,0,0,0,0,0,0,0,0};			// Holder for Page Response Times
-				String PTrun = "";		// Name of Performance Test Run
-				String startTime = "";	// Test start time
+				String [] nextLine;		// CSV line data container
 				int rowIterator = 0;	// Used to loop between rows
 				int colIterator = 0;	// Used to loop between columns
 				int rowCount = 0;		// Used to count the total number of rows
+				int pageCount = 0;
+				
+				String [] pageName = new String[100];	// Holder for Page names
+				double [] individualPRT = new double[100];	// Holder for Page Response Times
+				String PTrun = "";		// Name of Performance Test Run
+				String startTime = "";	// Test start time
+
 				double PRT = 0;			// Average Page Response Time
 		        double PRd = 0;			// Page Response Time Standard Deviation
 		        double ERT = 0;			// Average Element Response Time
@@ -89,34 +96,28 @@ public class CSV_ReportingConsolidator {
 				}
 		        
 		        // Read the page titles:
-		        while (colIterator < 9)	{
-		        	Page[colIterator] = nextLine[colIterator+18];
-		        	colIterator++;
+		        while (colIterator != -1)	{
+		        	pageName[colIterator] = nextLine[colIterator+18];
+		        	if((pageName[colIterator].equals(pageName[0])) && colIterator > 0)	{
+		        		pageCount = colIterator;
+		        		pageName[colIterator] = null;
+		        		colIterator = -1;	// Detects when the page titles start to repeat
+		        	}
+		        	else	{
+		        		colIterator++;
+		        	}
 		        }
-		        		        
-		        // Skip all rows except the last row containing summary data:
+		        
+		        // Retrieve non-continuous performance data
 		        nextLine = csvFile.readNext();
 		        nextLine = csvFile.readNext();
 		        while (rowIterator < rowCount-4) {
-		        	// Attempt to retrieve the summary data in the last rows, 3 rows allowance:
-			        if (nextLine[10].length() != 0)	{
-			        	PRT = Double.parseDouble(nextLine[10]);
-			        }	
-			        if (nextLine[11].length() != 0)	{
-						PRd = Double.parseDouble(nextLine[11]);
-			        }
-			        if (nextLine[16].length() != 0)	{
-						ERT = Double.parseDouble(nextLine[16]);
-			        }
-			        if (nextLine[17].length() != 0)	{
-						ERd = Double.parseDouble(nextLine[17]);
-			        }
-			        if (nextLine[12].length() != 0)	{
-						MRT = Integer.parseInt(nextLine[12]);
-		        	}
-					if (nextLine[13].length() != 0)	{
-						mRT = Integer.parseInt(nextLine[13]);
-			        }
+			        if (nextLine[10].length() != 0)	{	PRT = Double.parseDouble(nextLine[10]);		}	
+			        if (nextLine[11].length() != 0)	{	PRd = Double.parseDouble(nextLine[11]);		}
+			        if (nextLine[16].length() != 0)	{	ERT = Double.parseDouble(nextLine[16]);		}
+			        if (nextLine[17].length() != 0)	{	ERd = Double.parseDouble(nextLine[17]);		}
+			        if (nextLine[12].length() != 0)	{	MRT = Integer.parseInt(nextLine[12]);		}
+					if (nextLine[13].length() != 0)	{	mRT = Integer.parseInt(nextLine[13]);		}
 					nextLine = csvFile.readNext();
 					rowIterator++;
 				}
@@ -153,9 +154,9 @@ public class CSV_ReportingConsolidator {
 				
 				// Dynamically retrieve individual page response times, correlate with page names:
 				while (rowIterator < rowCount)	{
-					while (colIterator < 9)	{
+					while (colIterator < pageCount)	{
 						if (nextLine[colIterator+18].length() != 0)	{
-							PagePRT[colIterator] = Double.parseDouble(nextLine[colIterator+18]);
+							individualPRT[colIterator] = Double.parseDouble(nextLine[colIterator+18]);
 						}
 						colIterator++;
 					}
@@ -193,6 +194,16 @@ public class CSV_ReportingConsolidator {
 				}
 				*/
 				
+				// Serialize individual Page Response Times into CSV values
+				StringBuffer individualPRTList = new StringBuffer();
+				if(individualPRT.length > 0){
+					individualPRTList.append(String.valueOf(individualPRT[0]));
+					for(int k=1;k<pageCount;k++)	{
+						individualPRTList.append(",");
+						individualPRTList.append(String.valueOf(individualPRT[k]));
+					}
+				}
+				
 				// Serialize all retrieved performance parameters:
 				String[] entries = {PTrun,startTime,
 						String.valueOf(completedUsers),
@@ -207,21 +218,23 @@ public class CSV_ReportingConsolidator {
 						String.valueOf(TEH),
 						String.valueOf(ERT),
 						String.valueOf(ERd),"",
-						String.valueOf(PagePRT[0]),
-						String.valueOf(PagePRT[1]),
-						String.valueOf(PagePRT[2]),
-						String.valueOf(PagePRT[3]),
-						String.valueOf(PagePRT[4]),"",
-						String.valueOf(PagePRT[5]),
-						String.valueOf(PagePRT[6]),
-						String.valueOf(PagePRT[7]),
-						String.valueOf(PagePRT[8]),
+						individualPRTList.toString(),
 						};
 				
-				writer.writeNext(entries);	// Write performance parameters in CSV format
+				// Define header and write it to the first CSV row
+				Object[] headerConcatenator = ArrayUtils.addAll(referenceHeaders,pageName);
+				String[] header = new String[referenceHeaders.length+pageCount];
+				header = Arrays.copyOf(headerConcatenator, header.length, String[].class);
+				
+				if(i==0)	{
+					writer.writeNext(header);	// Write CSV header
+				}
+				writer.writeNext(entries);		// Write performance parameters in CSV format
+				System.out.println("== Processed: " + filename + " ===========================");
 			}
 		}
-		writer.close();						// Close the CSV writer
-		
+		writer.close();							// Close the CSV writer
+		System.out.println(  "\n-- Done processing " + listOfFiles.length + " files."
+							+"\n-- The consolidated report has been saved to " + outputPath + outputFile);
 	}
 }
